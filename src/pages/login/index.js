629bc9ca84b9d8409d23c59963b88abe001b38dd
Rakshita Kanwar <49356542+rakshitaaa@users.noreply.github.com>
2023-12-28 13:44:02 +0530

    second

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -3,12 +3,27 @@ import './style.scss'
 const Login = () => {
   return (
     <div className="login-container">
-      <div className="logo">
-        <img src="" alt="wepitch logo"/>
-      </div>
-      <div className="login-inputs">
-        
-      </div>
+      <div className="login-sub-container">
+          <div className="login-logo">
+            <img src="https://wepitch.tech/wp-content/themes/astra/content/dam/infosys-web/burger-menu/en/images/wepitch-logo.png" alt="wepitch logo"/>
+          </div>
+          
+            <form className="login-inputs">
+              <div className="form-inputs">
+                <div className="email-inputs">
+                  <label for="email">Email: </label>
+                  <input type="email" id="email" placeholder="Enter your email address" required />
+                </div>
+                <div className="password-inputs">
+                  <label for="password">Password: </label>
+                  <input type="password" id="password" placeholder="Enter your password" required />
+                </div>
+              </div>
+            </form>
+          <div className="login-other-links">
+            
+          </div>
+        </div>
     </div>
 
   )
